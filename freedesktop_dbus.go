943fde394f27dbdd0cde7e2175ca0145d0c00b7f
Hualet Wang <mr.asianwang@gmail.com>
2016-08-20 16:01:17 +0800

    Find service name from pid

--- freedesktop_dbus.go
+++ freedesktop_dbus.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strings"
 )
 
 type processID uint32
 
 var manager *dbus.DBusDaemon
-var knownAddress map[Address]processID
+var addressProcMap map[Address]processID
+var procServiceMap map[processID]string
 
 func init() {
 	var err error
@@ -20,19 +22,39 @@ func init() {
 		panic(err)
 	}
 
-	knownAddress = make(map[Address]processID)
+	addressProcMap = make(map[Address]processID)
+	procServiceMap = make(map[processID]string)
+
 	allNames, err := manager.ListNames()
 	if err != nil {
 		log.Print("ListNames failed :", err)
 	}
 	for _, name := range allNames {
 		id := processIDFromBusAddress(Address(name))
-		knownAddress[Address(name)] = processID(id)
+		addressProcMap[Address(name)] = processID(id)
+
+		tryUpdateProcServiceMap(processID(id), name)
 	}
 }
 
+func tryUpdateProcServiceMap(pid processID, name string) {
+	// we just want service names, not dbus addresses.
+	if !strings.HasPrefix(name, ":") {
+		procServiceMap[pid] = name
+	}
+}
+
+func serviceNameFromProcessID(pid processID) string {
+	v, ok := procServiceMap[pid]
+	if ok {
+		return v
+	}
+
+	return ""
+}
+
 func processIDFromBusAddress(addr Address) processID {
-	v, ok := knownAddress[addr]
+	v, ok := addressProcMap[addr]
 	if ok {
 		return v
 	}
@@ -43,17 +65,22 @@ func processIDFromBusAddress(addr Address) processID {
 	}
 
 	result := processID(id)
-	knownAddress[addr] = result
+	addressProcMap[addr] = result
+	tryUpdateProcServiceMap(result, string(addr))
 
 	return result
 }
 
-func processNameFromBusAddress(addr Address) string {
-	pid := processIDFromBusAddress(addr)
+func processNameFromProcessID(pid processID) string {
 	name, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
 	if err != nil {
 		log.Print("get process name error: ", err)
 	}
 
 	return string(name)
+}
+
+func processNameFromBusAddress(addr Address) string {
+	pid := processIDFromBusAddress(addr)
+	return processNameFromProcessID(pid)
 }
